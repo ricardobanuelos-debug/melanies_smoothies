3e5faf8f96e24030f3b52f32b4115ea8203af238
ricardobanuelos-debug <ricardo.banuelos@softtek.com>
2025-08-12 17:24:42 -0600

    Update streamit_app.py
    New Changes of Course

--- streamit_app.py
+++ streamit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col 
 
 # Write directly to the app
@@ -29,6 +30,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_dt = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -46,8 +49,4 @@ if ingredients_list:
 else:
     st.info("Pick up to 5 ingredients to continue.")
 
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_dt = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+

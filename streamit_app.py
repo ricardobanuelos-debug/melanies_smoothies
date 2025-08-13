42d0ef86998f4ebad887797632bdbe0989b4dfbc
ricardobanuelos-debug <ricardo.banuelos@softtek.com>
2025-08-12 18:18:17 -0600

    Update streamit_app.py
    Prueba de nueva columna

--- streamit_app.py
+++ streamit_app.py
@@ -16,8 +16,9 @@ st.write("The name on your Smoothie will be:", name_of_order)
 cnx = st.connection("Snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Chose up to 5 ingredients:'
